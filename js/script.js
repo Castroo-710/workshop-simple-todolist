297276a4505747ad717141cbaea8de3955c40d3e
Cristian Castro <cristian.castro@hotmail.se>
2025-10-23 15:00:06 +0200

    klar to do list

--- js/script.js
+++ js/script.js
@@ -1,36 +1,87 @@
 // 1. DOM referenser
 const todoList = document.getElementById("todos");
+const input = document.getElementById("input");
+const todoForm = document.getElementById("todoForm");
+
 
 
 
 // 2. Eventlyssnare - När ett formulär submittas anropa funktion addTodo()
-
-
+document.addEventListener("submit", addTodo);
 
 
 
 // 3. Börja med en tom array på todos
 // samt ett id som börjar på noll
-
+let tasks = [];
+let id = 0;
 
 
 
 
 // 4. Lägg till en Todo som ett objekt  i arrayen todos
-function addTodo() {
+function addTodo(event) {
+    const text = input.value.trim();
+    event.preventDefault();
+    if (!text) return;
+    const newTodo = {
+        id: id++,
+        text,
+        completed: false,
+    };
 
-    
+    tasks.push(newTodo);
+    input.value = "";
+    renderList();
 }
 
 
 
 // 5. Funktion som renderar listan på index.html varje gång listan förändraas
 function renderList() {
+    todoList.innerHTML = "";
 
-    // Iterera genom varje list-element och skapa en <li></li> med todo-texten, samt en close-knapp
+    tasks.forEach((todo, index) => {
+    const li = document.createElement("li");
+    li.dataset.id = String(todoForm.id);
+    li.dataset.id = index;
+    li.className = "todo-item";
+    
+    const checkbox = document.createElement("input");
+    checkbox.type = "checkbox";
+    checkbox.checked = todo.completed;
+    checkbox.className = "todo-checkbox";
 
+    checkbox.addEventListener("change", () => {
+        todo.completed = checkbox.checked;
+        renderList();
+    });
+    
+    const textSpan = document.createElement("span");
+    textSpan.className = "todo-text";
+    textSpan.textContent = todo.text;
+    if (todo.completed) {
+        textSpan.classList.add("completed");
+        }
+
+
+        const removeBtn = document.createElement("button");
+        removeBtn.textContent = "❌";
+        removeBtn.className = "remove-btn btn-danger";
+
+        removeBtn.addEventListener("click", () =>{
+            tasks.splice(index, 1);
+            renderList();
+        });
+        
+        li.appendChild(checkbox);
+        li.appendChild(textSpan);
+        li.appendChild(removeBtn);
+        todoList.appendChild(li);
+        
+        
+    });
 }
-
 
 
 
@@ -48,7 +99,11 @@ todoList.addEventListener('click', function(event) {
 
 // Bonus! Uppdatera en todo med visst id. Toggle "completed"
 function toggleTodoCompletion(id) {
-
+    const todo = todoList.firstElementChild((t) => t.id === id);
+    if (todo) {
+        todo.completed = !todo.completed;
+        renderList();
+    }
 
 }
 
@@ -56,7 +111,8 @@ function toggleTodoCompletion(id) {
 
 // Bonus
 function deleteTodo(id) {
-  
+  todos = todos.filter((t) => t.id !== id);
+  renderList();
 }
 
 
